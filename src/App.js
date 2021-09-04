ff350d4faab30fdfae9cfaca36a83c62cfe253c2
titevar <59051464+titevar@users.noreply.github.com>
2021-09-04 22:29:55 +0400

    added collection items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import ShopPage from './pages/shop/shop.component'
+
 import HomePage from './pages/homepage/homepage.component'
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <HomePage />
+      </Switch>
     </div>
   );
 }
