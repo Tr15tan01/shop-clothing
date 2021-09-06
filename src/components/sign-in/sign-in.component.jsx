f1be6277fbb11dbd628cb5b6151937fd817b02ea
titevar <59051464+titevar@users.noreply.github.com>
2021-09-06 23:42:58 +0400

    added sign up and sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 import './sign-in.styles.scss'
 
@@ -16,9 +16,16 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault()
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     handleChange = event => {
@@ -32,7 +39,7 @@ class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email' type='email' value={this.state.email} label='email' handleChange={this.handleChange} required autocomplete="off" />
+                    <FormInput name='email' type='email' value={this.state.email} label='email' handleChange={this.handleChange} required autoComplete="off" />
 
                     <FormInput name='password' type='password' value={this.state.password} label='password' handleChange={this.handleChange} required />
                     <div className="buttons">
